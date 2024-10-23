23922d14c4b5547b00bddcc2eac38ddb45835e5a
Yassine Bouanane <ybouanan@c3r2p4.1337.ma>
2024-10-23 17:45:52 +0100

    hhh

---
+++ tmp/ft_strdup.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybouanan <ybouanan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 15:00:37 by ybouanan          #+#    #+#             */
+/*   Updated: 2024/10/23 15:36:41 by ybouanan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <libft.h>
+
+char	*ft_strdup(const char *s)
+{
+	size_t	c;
+	size_t	lent;
+	char	*tmp;
+
+	lent = ft_strlen(s);
+	c = 0;
+	if (lent == 0)
+		return (NULL);
+	tmp = (char *)malloc(lent + 1);
+	if (tmp == NULL)
+		return (NULL);
+	while (c < lent)
+	{
+		tmp[c] = s[c];
+		c++;
+	}
+	tmp[lent] = '\0';
+	return (tmp);
+}
